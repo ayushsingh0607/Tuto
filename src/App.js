64634bae3c7683082bbfe95e3209ef9c2d9369bd
ayushsingh0607 <ayushraj0607@gmail.com>
2022-04-15 22:35:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from "./Navbar"
+import Home from "./Home"
+function App() {
+  // const title="Welcome to the new blog";
+  // const link="https://www.google.com";
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+        
+         </div>
+        </div>
+    
+  );
+
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+const Navbar=() => {
+  return (
+      
+      <nav className="navbar">
+             <h1>The Travel Blog</h1>
+             <div className="links">
+                 <a href="/"> Home </a>
+                 <a href="/create" >New Blog</a>
+           </div>
+         </nav>
+      
+   
+
+  );
+}
+
+export default Navbar
+
+
