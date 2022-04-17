2ea49cf7849ecc97df19bc3c52d742f02b55a9fc
ayushsingh0607 <ayushraj0607@gmail.com>
2022-04-17 13:19:17 +0530

    qwerty

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,21 @@
 import React from 'react' 
 import {useState} from 'react'
-const Home=()=> {
-    const[blogs, setBlogs ]=useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-  return (
-    <div className='home'>
-      {blogs.map(blog => (
-       <div className="blog-preview" key={blog.id}>
-       <h2>{blog.title}</h2>
-        <p>Written by {blog.author}</p>
-        </div>
-      ))}
-       
-    
-    </div>
+import Bloglist from './Bloglist'
+function Home() {
+  const[blogs, setBlogs]=useState([
+          { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+         ]);
+         const handleDelete =(id)=>{
+           const newblogs=blogs.filter(blog=>blog.id!==id);
+            setBlogs(newblogs);
+         }
+       return (
+         <div className='home'>
+           <Bloglist blogs={blogs} title="All Blogs!" handleDelete ={handleDelete } />
+           {/* <Bloglist blogs={blogs.filter(blog => blog.author==='mario')} title="mario's Blogs" />  */}
+         </div>
   )
 }
 
